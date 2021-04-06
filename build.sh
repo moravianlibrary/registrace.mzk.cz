5e14056d7451d900f79e78a7d048154aaa3fb21a
Kaplan Věroš <veros.kaplan@mzk.cz>
2021-04-06 14:19:36 +0000

    feat: Apache status fixed
    Signed-off-by: Jakub Vokoun <jakub.vokoun@gmail.com>

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@ set -x
 set -e
 docker build -t moravianlibrary/registrace-php:build -f build/php-image/Dockerfile build/php-image
 docker image tag moravianlibrary/registrace-php:build registry.app.knihovny.cz/moravianlibrary/registrace-php:build
-docker image tag registry.app.knihovny.cz/moravianlibrary/registrace-php:build docker.app.knihovny.cz/moravianlibrary/registrace-php:build 
+docker image tag registry.app.knihovny.cz/moravianlibrary/registrace-php:build docker.app.knihovny.cz/moravianlibrary/registrace-php:build
 docker image push registry.app.knihovny.cz/moravianlibrary/registrace-php:build
-#
-docker build -t moravianlibrary/registrace:build -fbuild/app/Dockerfile . 
-#
+docker build -t moravianlibrary/registrace:build -f build/app/Dockerfile .
 docker image tag moravianlibrary/registrace:build registry.app.knihovny.cz/moravianlibrary/registrace:build
 docker image push registry.app.knihovny.cz/moravianlibrary/registrace:build
