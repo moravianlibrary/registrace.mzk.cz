6c093dbbe881983996d0177966d5b9921562bd83
Vaclav Rosecky <xrosecky@gmail.com>
2021-04-07 18:08:30 +0200

    script run.sh for local development

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+set -e
+docker build -t moravianlibrary/registrace-php:build -f build/php-image/Dockerfile build/php-image
+docker build -t moravianlibrary/registrace:build -f build/app/Dockerfile .
+docker-compose up
