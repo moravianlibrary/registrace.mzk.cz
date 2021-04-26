77ce81b9add23a861c6ba35d49b414240ec76b0b
Vaclav Rosecky <xrosecky@gmail.com>
2021-04-26 12:46:55 +0200

    improved implementation of IDS JMK discount

--- web/module/Registration/src/Service/DiscountService.php
+++ web/module/Registration/src/Service/DiscountService.php
@@ -9,6 +9,12 @@ use Registration\Form\UserForm;
 class DiscountService
 {
 
+    protected const IDS_JMK_NO_DISCOUNT = 1;
+
+    protected const IDS_JMK_DISCOUNT = 2;
+
+    protected const IDS_JMK_NOT_APPLICABLE = 3;
+
     protected const DEFAULT_AGE = 26;
 
     protected const MIN_AGE = 15;
@@ -48,20 +54,12 @@ class DiscountService
         $preferred = $this->findDiscountByAge($user);
         // only add cheaper discounts
         $discounts = [];
+        $idsJmk = $user->get('user')->get('idsJmk')->getValue();
         foreach ($this->discounts as $code => $discount) {
             if ($this->validate($discount, $user) &&
                 ($preferred == null
                     || $preferred['price'] > $discount['price']
                     || $preferred == $discount)) {
-                $price = $discount['price'];
-                if ($price > 0 && $user != null && $user->get('user')
-                        ->get('idsJmk')->getValue()) {
-                    $price = $price * 0.9;
-                }
-                $label = $discount['label'];
-                $label = str_replace('$price', $price, $label);
-                $discount['label'] = $label;
-                $discount['price'] = $price;
                 $discounts[$code] = $discount;
             }
         }
@@ -84,8 +82,16 @@ class DiscountService
         $this->discounts = [
             // default - no discount
             'NONE' => [
-                'label' => $this->translator->translate('discount_none'),
-                'price' => 200,
+                'label'   => $this->translator->translate('discount_none'),
+                'price'   => 200,
+                'ids_jmk' => self::IDS_JMK_NO_DISCOUNT,
+                'group'   => 'NONE',
+            ],
+            'NONE_IDS_JMK' => [
+                'label'   => $this->translator->translate('discount_none_ids_jmk'),
+                'price'   => 180,
+                'ids_jmk' => self::IDS_JMK_DISCOUNT,
+                'group'   => 'NONE',
             ],
             // by age limits
             'TEENAGER' => [
@@ -94,12 +100,24 @@ class DiscountService
                 'min_age'  => self::MIN_AGE,
                 'max_age'  => 19,
                 'only_age' => true,
+                'ids_jmk'  => self::IDS_JMK_NOT_APPLICABLE,
+                'group'    => 'TEENAGER',
             ],
             'UNIVERSITY_STUDENT' => [
                 'label'    => $this->translator->translate('discount_university_student'),
                 'price'    => 100,
                 'min_age'  => 19,
                 'max_age'  => 25,
+                'ids_jmk'  => self::IDS_JMK_NO_DISCOUNT,
+                'group'    => 'UNIVERSITY_STUDENT',
+            ],
+            'UNIVERSITY_STUDENT_IDS_JMK' => [
+                'label'    => $this->translator->translate('discount_university_student_ids_jmk'),
+                'price'    => 90,
+                'min_age'  => 19,
+                'max_age'  => 25,
+                'ids_jmk'  => self::IDS_JMK_DISCOUNT,
+                'group'    => 'UNIVERSITY_STUDENT',
             ],
             'SENIOR' => [
                 'label'    => $this->translator->translate('discount_senior'),
@@ -107,6 +125,8 @@ class DiscountService
                 'min_age'  => 65,
                 'max_age'  => 69,
                 'only_age' => true,
+                'ids_jmk'  => self::IDS_JMK_NOT_APPLICABLE,
+                'group'    => 'SENIOR',
             ],
             'OLD_SENIOR' => [
                 'label'    => $this->translator->translate('discount_old_senior'),
@@ -114,23 +134,34 @@ class DiscountService
                 'min_age'  => 70,
                 'max_age'  => self::MAX_AGE,
                 'only_age' => true,
+                'ids_jmk'  => self::IDS_JMK_NOT_APPLICABLE,
+                'group'    => 'OLD_SENIOR',
             ],
             // free
             'UNOB' => [
                 'label'    => $this->translator->translate('discount_unob'),
                 'price'    => 0,
                 'min_age'  => 19,
+                'ids_jmk'  => self::IDS_JMK_NOT_APPLICABLE,
+                'group'    => 'UNOB',
             ],
             'ZTP' => [
                 'label'    => $this->translator->translate('discount_ztp'),
                 'price'    => 0,
                 'min_age'  => 19,
+                'ids_jmk'  => self::IDS_JMK_NOT_APPLICABLE,
+                'group'    => 'ZTP',
             ],
         ];
     }
 
     protected function validate($discount, ?UserForm $user)
     {
+        $idsJmk = $user->get('user')->get('idsJmk')->getValue();
+        if (($idsJmk && $discount['ids_jmk'] == self::IDS_JMK_NO_DISCOUNT) ||
+            (!$idsJmk && $discount['ids_jmk'] == self::IDS_JMK_DISCOUNT)) {
+            return false;
+        }
         $age = self::DEFAULT_AGE;
         if ($user != null) {
             $age = $user->getAge() ?? self::DEFAULT_AGE;
