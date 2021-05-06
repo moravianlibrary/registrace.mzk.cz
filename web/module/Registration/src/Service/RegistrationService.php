cb4ebe9778217d0aebd28f753c91d1f559f7b7b4
Vaclav Rosecky <xrosecky@gmail.com>
2021-05-06 18:17:27 +0200

    registration service - demo mode, finding new unique user ID, update support

--- web/module/Registration/src/Service/RegistrationService.php
+++ web/module/Registration/src/Service/RegistrationService.php
@@ -12,17 +12,36 @@ class RegistrationService
 
     const XML_TEMPLATE = __DIR__. '/../../resources/registration.xml';
 
+    const CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    const PROTECTED_PARAMETERS = [
+        'xml_full_req',
+        'user_name',
+        'user_password',
+    ];
+
+    /** @var string */
     protected $url;
 
+    /** @var string */
     protected $user;
 
+    /** @var string */
     protected $password;
+
+    /** @var string */
+    protected $library;
+
+    /** @var string */
+    protected $demo;
 
     public function __construct($config)
     {
         $this->url = $config['aleph']['url'];
         $this->user = $config['aleph']['user'] ?? null;
         $this->password = $config['aleph']['password'] ?? null;
+        $this->library = $config['aleph']['library'] ?? 'MZK50';
+        $this->demo = $config['aleph']['demo'] ?? false;
     }
 
     public function register(User $user)
@@ -32,6 +51,7 @@ class RegistrationService
         $expiry = date('Ymd', strtotime('+1 year'));
         $xml = simplexml_load_file(self::XML_TEMPLATE);
         $patron = $xml->{'patron-record'}[0];
+        $recordAction = ($this->demo) ? 'U' : 'I';
         // z303
         $z303 = $patron->{'z303'};
         $z303->{'match-id'} = $id;
@@ -46,9 +66,11 @@ class RegistrationService
         $z303->{'z303-delinq-n-1'} = 'Online předregistrace';
         $z303->{'z303-delinq-1-update-date'} = $now;
         // test
-        $z303->{'z303-delinq-2'} = '88';
-        $z303->{'z303-delinq-n-2'} = 'Testovací registrace';
-        $z303->{'z303-delinq-2-update-date'} = $now;
+        if ($this->demo) {
+            $z303->{'z303-delinq-2'} = '88';
+            $z303->{'z303-delinq-n-2'} = 'Testovací registrace';
+            $z303->{'z303-delinq-2-update-date'} = $now;
+        }
         // z304
         $index = 0;
         foreach ($patron->{'z304'} as $z304) {
@@ -58,7 +80,7 @@ class RegistrationService
                 unset($patron->{'z304'}[1]);
                 break;
             }
-            $z304->{'record-action'} = 'U'; // for testing
+            $z304->{'record-action'} = $recordAction;
             $z304->{'z304-id'} = $id;
             $z304->{'z304-date-from'} = $now;
             $z304->{'email-address'} = $user->getEmail();
@@ -95,45 +117,129 @@ class RegistrationService
             $entry->{'z308-key-data'} = $id;
             $entry->{'z308-verification'} = $user->getPassword();
         }
-        // for testing
-        $z303->{'record-action'} = 'U';
-        $z305->{'record-action'} = 'U';
-        $result = $xml->asXML();
-        $this->getLogger()->info("XML for registration:\n" . $result);
-        $client = new Client();
-        $client->setUri($this->url);
-        $parameters = [
-            'op' => 'update_bor',
-            'library' => 'MZK50',
-            'update-flag' => 'Y',
-            'xml_full_req' => $result,
-        ];
-        if ($this->user && $this->password) {
-            $parameters['user_name'] = $this->user;
-            $parameters['user_password'] = $this->password;
-        }
-        $client->setParameterPost($parameters);
-        $client->setMethod('POST');
-        $response = $client->send();
-        if ($response->getStatusCode() != '200') {
-            throw new \Exception("Operation update_bor failed");
-        }
-        $this->getLogger()->info("User $id registered, response XML:\n"
-            . $response->getBody());
-        $xml = simplexml_load_string($response->getBody());
-        if (!$xml) {
-            throw new \Exception("Operation update_bor returned invalid XML");
-        }
-        $patronId = $xml->{'patron-id'} ?? null;
+        $z303->{'record-action'} = $recordAction;
+        $z305->{'record-action'} = $recordAction;
+        $request = $xml->asXML();
+        $this->getLogger()->info("XML for registration:\n" . $request);
+        $response = $this->updateUser($request);
+        $patronId = (string) $response->{'patron-id'} ?? null;
         if ($patronId == null) {
             throw new \Exception("Operation update_bor returned invalid XML");
         }
         return $id;
     }
 
+    public function updateUserAfterRegistration($patronId)
+    {
+        $patron = $this->findUser($patronId);
+        $z303 = $patron->{'z303'};
+        $update = new \SimpleXMLElement("<p-file-20><patron-record></patron-record></p-file-20>");
+        $z303Update = $update->{'patron-record'}->addChild('z303');
+        $z303Update->addChild('match-id-type', '00');
+        $z303Update->addChild('match-id', $patronId);
+        $z303Update->addChild('record-action', 'U');
+        foreach ($z303->children() as $child) {
+            $z303Update->addChild($child->getName(), (string) $child);
+        }
+        $xml = $update->asXml();
+        $this->getLogger()->info("XML after update:\n" . $xml);
+        return $this->updateUser($xml);
+    }
+
+    protected function findUser($patronId)
+    {
+        return $this->callXServer([
+            'op' => 'bor_info',
+            'bor_id' => $patronId,
+            'library' => 'MZK50',
+        ]);
+    }
+
+    protected function updateUser($xml)
+    {
+        $parameters = [
+            'op' => 'update_bor',
+            'library' => 'MZK50',
+            'update-flag' => 'Y',
+            'xml_full_req' => $xml,
+        ];
+        if ($this->user && $this->password) {
+            $parameters['user_name'] = $this->user;
+            $parameters['user_password'] = $this->password;
+        }
+        $xml = $this->callXServer($parameters, 'POST');
+        return $xml;
+    }
+
+    protected function callXServer($parameters, $method='GET') {
+        $operation = $parameters['op'];
+        $this->logUrl($parameters);
+        $client = new Client();
+        $client->setUri($this->url);
+        if ($method == 'POST') {
+            $client->setParameterPost($parameters);
+        } else {
+            $client->setParameterGet($parameters);
+        }
+        $client->setMethod($method);
+        $response = $client->send();
+        if ($response->getStatusCode() != '200') {
+            throw new \Exception("Operation $operation failed");
+        }
+        $this->getLogger()->info("Response body: " . $response);
+        $xml = simplexml_load_string($response->getBody());
+        if (!$xml) {
+            throw new \Exception("Operation $operation returned invalid XML");
+        }
+        // special case: update_bor
+        // <error>Succeeded to WRITE table z303. cur-id XXXXXXXXXXXX.</error>
+        // <error>Succeeded to REWRITE table z303. cur-id XXXXXXXXXXXX.</error>
+        if ($operation == 'update_bor') {
+            foreach ($xml->error as $error) {
+                if (!(strpos($error, 'Succeeded to') === 0)) {
+                    throw new \Exception($error);
+                }
+            }
+            return $xml;
+        }
+        if (isset($xml->error)) {
+            throw new \Exception($xml->error);
+        }
+        return $xml;
+    }
+
+    protected function logUrl($parameters)
+    {
+        foreach (self::PROTECTED_PARAMETERS as $protected) {
+            if (isset($parameters[$protected])) {
+                $parameters[$protected] = "***";
+            }
+        }
+        $params = http_build_query($parameters);
+        $this->getLogger()->info("About to call URL: " .
+            $this->url . '?' . $params);
+    }
+
     protected function getId(User $user)
     {
-        return "MZKTEST";
+        if ($this->demo) {
+            return 'MZKTEST';
+        }
+        $count = strlen(self::CHARACTERS);
+        while (true) {
+            $suffix = '';
+            for ($i = 1; $i <= 10; $i++) {
+                $suffix .= self::CHARACTERS[mt_rand(0, $count - 1)];
+            }
+            $userId = $user->getBirth()->format("Ymd") . $suffix;
+            try {
+                $this->findUser($userId);
+            } catch (\Exception $ex) {
+                if ($ex->getMessage() == 'Error retrieving Patron System Key') {
+                    return $userId;
+                }
+            }
+        }
     }
 
 }
