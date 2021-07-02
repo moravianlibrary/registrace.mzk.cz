524856e44a54f5c319a8418c7b2c906137f6ab17
Vaclav Rosecky <xrosecky@gmail.com>
2021-07-02 09:43:54 +0200

    improved support for eduid.cz

--- web/module/Registration/src/IdentityProvider/EduId.php
+++ web/module/Registration/src/IdentityProvider/EduId.php
@@ -7,6 +7,15 @@ use Laminas\Http\PhpEnvironment\Request;
 class EduId implements IdentityProviderInterface
 {
 
+    const REQUIRED_ATTRIBUTES = [
+        'firstName',
+        'lastName',
+        'street',
+        'city',
+        'postcode',
+        'country',
+    ];
+
     public function identify(Request $request)
     {
         $result = [
@@ -21,15 +30,26 @@ class EduId implements IdentityProviderInterface
                 'street' => $this->get($request, 'street'),
                 'city' => $this->get($request, 'city'),
                 'postcode' => $this->get($request, 'postcode'),
+                'country' => $this->get($request, 'country'),
             ],
-            'verified' => false,
         ];
+        $result['verified'] = $this->hasAllRequiredAttributes($request);
         return $result;
     }
 
-    public function get(Request $request, string $variable)
+    protected function get(Request $request, string $variable)
     {
         return $request->getServer($variable);
+    }
+
+    protected function hasAllRequiredAttributes(Request $request)
+    {
+        foreach (self::REQUIRED_ATTRIBUTES as $attr) {
+            if (empty($this->get($request, $attr))) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
