91175224008bf2404e6bd8ced3d2b4dde3aa04d7
Vaclav Rosecky <xrosecky@gmail.com>
2021-07-12 19:47:56 +0200

    shibboleth certificate moved to configuration

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
 set -e
-docker build -t moravianlibrary/registrace/php:build -f build/php-image/Dockerfile build/php-image
+docker build --no-cache	-t moravianlibrary/registrace/php:build -f build/php-image/Dockerfile build/php-image
 docker image tag moravianlibrary/registrace/php:build registry.app.knihovny.cz/moravianlibrary/registrace/php:build
 docker image tag registry.app.knihovny.cz/moravianlibrary/registrace/php:build docker.app.knihovny.cz/moravianlibrary/registrace/php:build
 docker image push registry.app.knihovny.cz/moravianlibrary/registrace/php:build
-docker build -t moravianlibrary/registrace/app:build -f build/app/Dockerfile .
+docker build --no-cache	-t moravianlibrary/registrace/app:build -f build/app/Dockerfile .
 docker image tag moravianlibrary/registrace/app:build registry.app.knihovny.cz/moravianlibrary/registrace/app:build
 docker image push registry.app.knihovny.cz/moravianlibrary/registrace/app:build
