3c4159a54872a96899b2f47f16a6cd4e0a51eb77
Vaclav Rosecky <xrosecky@gmail.com>
2021-11-18 07:57:14 +0100

    set z304-address-0 in Aleph

--- web/module/Registration/src/Service/RegistrationService.php
+++ web/module/Registration/src/Service/RegistrationService.php
@@ -115,6 +115,7 @@ class RegistrationService implements RegistrationServiceInterface
             $z304->{'email-address'} = $user->getEmail();
             $z304->{'z304-email-address'} = $user->getEmail();
             $z304->{'z304-telephone'} = $user->getPhone();
+            $z304->{'z304-address-0'} = ($index == 0) ? 'CARAK--' : 'CARAK';
             $z304->{'z304-address-1'} = $cn;
             $z304->{'z304-address-2'} = $address->getStreet();
             $city = $address->getPostcode() . ' ' . $address->getCity();
